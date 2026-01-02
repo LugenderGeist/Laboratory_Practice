ad0c4589639f53abba2e1cc008335eef18cb1c00
LugenderGeist <avk270@tpu.ru>
2026-01-02 17:00:55 +0700

    становление кнопок

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -6,6 +6,7 @@ int main(void)
 GPIO_Init();
 
     while(1){
+        if ((*(uint32_t *)(0x40020400 + 0x18)) & 
         GPIO |= GPIOEN0;
         GPIO |= GPIOEN1;
         GPIO |= GPIOEN2; // включение светодиода B2
