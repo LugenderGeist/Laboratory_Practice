76038706b5159aacab7c5789b97b6c3c3d55cfb3
LugenderGeist <avk270@tpu.ru>
2026-01-03 17:58:32 +0700

    убралась чуть-чуть

--- Core/Src/init.c
+++ Core/Src/init.c
@@ -1,35 +1,28 @@
 #include "init.h"
 
-void GPIO_Init (void){
-RCC_GPIO_ENF;
-RCC_GPIO_ENB;
-GPIOF_PPUP;
-GPIOB_PPUP;
+void GPIO_Init(void) {
+    RCC_GPIO_ENF;
+    RCC_GPIO_ENB;
+    GPIOF_PPUP;
+    GPIOB_PPUP;
 
-GPIOF_MODER |= GPIO_SET0;
-GPIOF_OSPEED |= GPIO_SET0;
-
-GPIOF_MODER |= GPIO_SET1;
-GPIOF_OSPEED |= GPIO_SET1;
-
-GPIOF_MODER |= GPIO_SET2;
-GPIOF_OSPEED |= GPIO_SET2;
-
-GPIOF_MODER |= GPIO_SET3;
-GPIOF_OSPEED |= GPIO_SET3;
-
-GPIOF_MODER |= GPIO_SET4;
-GPIOF_OSPEED |= GPIO_SET4;
-
-GPIOF_MODER |= GPIO_SET5;
-GPIOF_OSPEED |= GPIO_SET5;
+    uint32_t gpio_sets[] = {GPIO_SET0, GPIO_SET1, GPIO_SET2, GPIO_SET3, GPIO_SET4, GPIO_SET5};
+    for (int i = 0; i < 6; i++) {
+        GPIOF_MODER |= gpio_sets[i];
+        GPIOF_OSPEED |= gpio_sets[i];
+    }
 }
 
 void off_all_led(void) {
-    GPIO |= 0x10000;   // Выкл LED0
-    GPIO |= 0x20000;   // Выкл LED1
-    GPIO |= 0x40000;   // Выкл LED2
-    GPIO |= 0x80000;   // Выкл LED3
-    GPIO |= 0x100000;  // Выкл LED4
-    GPIO |= 0x200000;  // Выкл LED5
+    GPIO |= 0x10000;  // Выкл LED0
+    GPIO |= 0x20000;  // Выкл LED1
+    GPIO |= 0x40000;  // Выкл LED2
+    GPIO |= 0x80000;  // Выкл LED3
+    GPIO |= 0x100000; // Выкл LED4
+    GPIO |= 0x200000; // Выкл LED5
+}
+
+int read_button(uint8_t button_pin) {
+    if (button_pin == 0) {return 0;}
+    else {return 1;}
 }
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -5,79 +5,56 @@ int B1_prev;
 int B2;
 int B2_prev;
 
-int count_B1 = 1; // число светодиодов, горящих одновременно
-int LED_group = 0; //позиция группы светодиодов
-int LED_index; // индекс текущего светодиода
+int count_B1 = 1;      // число светодиодов, горящих одновременно
+int LED_group = 0;     // позиция группы светодиодов
+int LED_index;         // индекс текущего светодиода
 const int LED_num = 6; // всего светодиодов
 
-int main(void){
+int main(void) {
 
     GPIO_Init();
     off_all_led();
-    GPIO |= GPIOEN0;      // Включаем 1 светодиод
+    GPIO |= GPIO_SET0;
 
-    while(1){
+    while (1) {
         // Считывание первой кнопки
-        if ((GPIO_READ1 == 0)) { B1 = 0;}
-        if ((GPIO_READ1 != 0)) { B1 = 1;}  
+        B1 = read_button(GPIO_READ1);
 
         if (B1_prev == 0 && B1 == 1) {
+            count_B1++;
+            if (count_B1 > LED_num) {count_B1 = 1;} 
+            LED_group = 0;
+            off_all_led();
 
-            count_B1++; // count_B1 по сути число светодиодов, которые включаются
-            if (count_B1 > LED_num) {count_B1 = 1;} // сдвиг от 1 до 6
-                LED_group = 0;
-                off_all_led(); // все выключаем
-
-                for (int i = 0; i < count_B1; i++) // выбираем число светодиодов
-                {
-                     LED_index = i;
-                    GPIO |= (0x01 << LED_index);
-                }
+            for (int i = 0; i < count_B1; i++) { // выбираем число светодиодов 
+                LED_index = i;
+                GPIO |= (0x01 << LED_index);
+            }
         }
         B1_prev = B1;
 
-// Считывание второй кнопки
-if ((GPIO_READ2 == 0)) { B2 = 0;}
-if ((GPIO_READ2 != 0)) { B2 = 1;}
+        // Считывание второй кнопки
+        B2 = read_button(GPIO_READ2);
 
-// Обработка нажатия второй кнопки
-if (B2_prev == 0 && B2 == 1)
-{
-    // Увеличиваем позицию группы на количество светодиодов
-    LED_group += count_B1;
-    
-    // Проверяем, не вышла ли группа за пределы
-    if (LED_group >= LED_num)
-    {
-        // Если вышли за пределы - ВЫКЛЮЧАЕМ ВСЕ светодиоды
-        GPIO |= 0x10000;   // Выкл LED0
-        GPIO |= 0x20000;   // Выкл LED1  
-        GPIO |= 0x40000;   // Выкл LED2
-        GPIO |= 0x80000;   // Выкл LED3
-        GPIO |= 0x100000;  // Выкл LED4
-        GPIO |= 0x200000;  // Выкл LED5
-        
-        // Сбрасываем позицию группы
-        LED_group = 0;
-        
-        // И ВКЛЮЧАЕМ первую группу снова
-        for (int i = 0; i < count_B1; i++)
-        {
-            LED_index = (LED_group + i) % LED_num;
-            GPIO |= (0x01 << LED_index);
+        if (B2_prev == 0 && B2 == 1) {
+            LED_group += count_B1; // увеличиваем позицию группы на количество светодиодов
+
+            if (LED_group >= LED_num) {
+                off_all_led();
+                LED_group = 0;
+                for (int i = 0; i < count_B1; i++) {
+                    LED_index = (LED_group + i) % LED_num;
+                    GPIO |= (0x01 << LED_index);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < count_B1; i++) {
+                    LED_index = (LED_group + i) % LED_num;
+                    GPIO |= (0x01 << LED_index);
+                }
+            }
         }
-    }
-    else
-    {
-        // Если еще в пределах - НЕ выключаем старые, 
-        // а только ДОБАВЛЯЕМ новую группу светодиодов
-        for (int i = 0; i < count_B1; i++)
-        {
-            LED_index = (LED_group + i) % LED_num;
-            GPIO |= (0x01 << LED_index);
-        }
-    }
-}
-B2_prev = B2;
+        B2_prev = B2;
     }
 }
