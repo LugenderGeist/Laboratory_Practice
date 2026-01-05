20cb1de671e93096f20ea7ba23ee71fd8513bc7a
LugenderGeist <avk270@tpu.ru>
2026-01-05 10:22:20 +0700

    все работает

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -5,28 +5,33 @@ int B1_prev;
 int B2;
 int B2_prev;
 
-int count_B1 = 1;      // число светодиодов, горящих одновременно
-int LED_group = 0;     // позиция группы светодиодов
-int LED_index;         // индекс текущего светодиода
-const int LED_num = 6; // всего светодиодов
+int count_B1 = 1;       // число светодиодов, горящих одновременно
+int LED_group = 0;      // позиция группы светодиодов
+int LED_index;          // индекс текущего светодиода
+const int LED_num = 6;  // всего светодиодов в системе
+static int all_leds_on; // флаг, включен ыли все светодиоды
 
-int main(void) {
+int main(void)
+{
 
     GPIO_Init();
     off_all_led();
-    *(uint32_t *)(0x40021400 + 0x18) |= 0x01; //включение первого светодиода
+    *(uint32_t *)(0x40021400 + 0x18) |= 0x01; // включение первого
 
-    while (1) {
+    while (1)
+    {
         // Считывание первой кнопки
         B1 = read_button(GPIO_READ1);
 
-        if (B1_prev == 0 && B1 == 1) {
+        if (B1_prev == 0 && B1 == 1){
             count_B1++;
-            if (count_B1 > LED_num) {count_B1 = 1;} 
+            if (count_B1 > LED_num){
+                count_B1 = 1;
+            }
             LED_group = 0;
             off_all_led();
 
-            for (int i = 0; i < count_B1; i++) { // выбираем число светодиодов 
+            for (int i = 0; i < count_B1; i++){
                 LED_index = i;
                 GPIO |= (0x01 << LED_index);
             }
@@ -36,22 +41,41 @@ int main(void) {
         // Считывание второй кнопки
         B2 = read_button(GPIO_READ2);
 
-        if (B2_prev == 0 && B2 == 1) {
-            LED_group += count_B1; // увеличиваем позицию группы на количество светодиодов
+        if (B2_prev == 0 && B2 == 1){
+            if (count_B1 == LED_num){
+                all_leds_on = 0;
 
-            if (LED_group >= LED_num) {
-                off_all_led();
-                LED_group = 0;
-                for (int i = 0; i < count_B1; i++) {
-                    LED_index = (LED_group + i) % LED_num;
-                    GPIO |= (0x01 << LED_index);
+                if (all_leds_on == 0){
+                    off_all_led();
+                    for (int i = 0; i < LED_num; i++){
+                        GPIO |= (0x01 << i);
+                    }
+                    all_leds_on = 1;
+                    LED_group = 0;
+                }
+                else{
+                    off_all_led();
+                    all_leds_on = 0;
+                    LED_group = 0;
                 }
             }
-            else
-            {
-                for (int i = 0; i < count_B1; i++) {
-                    LED_index = (LED_group + i) % LED_num;
-                    GPIO |= (0x01 << LED_index);
+            else{
+                LED_group += count_B1;
+
+                if (LED_group >= LED_num){
+                    off_all_led();
+                    LED_group = 0;
+
+                    for (int i = 0; i < count_B1; i++){
+                        LED_index = i;
+                        GPIO |= (0x01 << LED_index);
+                    }
+                }
+                else{
+                    for (int i = 0; i < count_B1; i++){
+                        LED_index = (LED_group + i) % LED_num;
+                        GPIO |= (0x01 << LED_index);
+                    }
                 }
             }
         }
